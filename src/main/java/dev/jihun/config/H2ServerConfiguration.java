122cde53a4542fe463f129bb072fb00a9a6baf6b
JiHun Hong <hongji3354@gmail.com>
2022-02-04 11:55:37 +0900

    feat : profile이 local일 시 H2를 TCP Server로 구동하도록 변경

--- src/main/java/dev/jihun/config/H2ServerConfiguration.java
+++ src/main/java/dev/jihun/config/H2ServerConfiguration.java
@@ -5,10 +5,12 @@ import org.h2.tools.Server;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
 
 import javax.sql.DataSource;
 import java.sql.SQLException;
 
+@Profile("local")
 @Configuration
 public class H2ServerConfiguration {
 
